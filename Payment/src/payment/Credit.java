d8e0dff3a11c15704b576492e807009b617bf29d
ohamedkhaled <75912765+ohamedkhaled@users.noreply.github.com>
2021-04-24 02:48:48 +0200

    paymant

---
+++ Payment/src/payment/Credit.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package payment;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Credit extends Payment {
+    
+    private String name ,type;
+    
+}
